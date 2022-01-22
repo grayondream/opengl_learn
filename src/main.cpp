5119fda81b71391c3f687c4ad59e411fe9c2e261
grayondream <grayondream@outlook.com>
2022-01-22 22:48:28 +0800

    feat: 添加光源

--- src/main.cpp
+++ src/main.cpp
@@ -99,56 +99,23 @@ int main() {
 	glfwSetCursorPosCallback(pwin, mouse_callback);
 	glfwSetScrollCallback(pwin, scroll_callback);
 
-    string vshaderSource = "E:/code/opengllearn/glsl/vshader.vs";
-    string fshaderSource = "E:/code/opengllearn/glsl/fshader.fs";
-	Shader shader;
-	int ret = shader.open(vshaderSource.c_str(), fshaderSource.c_str());
+    string vobjShader = "E:/code/opengllearn/glsl/obj.vs";
+    string fobjShader = "E:/code/opengllearn/glsl/obj.fs";
+	string vlightShader = "E:/code/opengllearn/glsl/light.vs";
+	string flightShader = "E:/code/opengllearn/glsl/light.fs";
+	Shader objShader;
+	int ret = objShader.open(vobjShader.c_str(), fobjShader.c_str());
 	if (!ret) {
-		elog("can not create shader context");
+		elog("can not create objShader context");
 		exit(1);
 	}
 
-	//create texture
-	unsigned int textureId[2] = { 0 };
-	glGenTextures(2, textureId);
-	glBindTexture(GL_TEXTURE_2D, textureId[0]);
-
-	//set some text parameters
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-	int imageWidth = -1, imageHeight = -1, imageChannel = -1;
-	const char *imageFile = "../resources/panda.jpg";
-	stbi_set_flip_vertically_on_load(true);
-	unsigned char *imageData = stbi_load(imageFile, &imageWidth, &imageHeight, &imageChannel, 0);
-	if (!imageData) {
-		elog("can not read data from image %s", imageFile);
+	Shader lightShader;
+	ret = lightShader.open(vlightShader.c_str(), flightShader.c_str());
+	if (!ret) {
+		elog("can not create objShader context");
+		exit(1);
 	}
-
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, imageWidth, imageHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, imageData);
-	glGenerateMipmap(GL_TEXTURE_2D);
-	stbi_image_free(imageData);
-	imageData = nullptr;
-
-	glBindTexture(GL_TEXTURE_2D, textureId[1]);
-
-	//set some text parameters
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-	const char *pngFile = "../resources/awesomeface.png";
-	imageData = stbi_load(pngFile, &imageWidth, &imageHeight, &imageChannel, 0);
-	if (!imageData) {
-		elog("can not read data from image %s", pngFile);
-	}
-
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, imageWidth, imageHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, imageData);
-	glGenerateMipmap(GL_TEXTURE_2D);
-	stbi_image_free(imageData);
 
     //三角形的顶点数据
 	float vertices[] = {
@@ -216,31 +183,22 @@ int main() {
     //设置顶点属性指针
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);   
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-	glEnableVertexAttribArray(1);
 
-
-	shader.use(); // 不要忘记在设置uniform变量之前激活着色器程序！
-	glUniform1i(glGetUniformLocation(shader.getID(), "texture1"), 0); // 手动设置
-	shader.setInt("texture2", 1); // 或者使用着色器类设置
+	unsigned int lightVao = 0;
+	glGenVertexArrays(1, &lightVao);
+	glBindVertexArray(lightVao);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
 
     glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
     //glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
     //创建gl的view
 	glEnable(GL_DEPTH_TEST);
 
-	glm::vec3 cubePositions[] = {
-	  glm::vec3(0.0f,  0.0f,  0.0f),
-	  glm::vec3(2.0f,  5.0f, -15.0f),
-	  glm::vec3(-1.5f, -2.2f, -2.5f),
-	  glm::vec3(-3.8f, -2.0f, -12.3f),
-	  glm::vec3(2.4f, -0.4f, -3.5f),
-	  glm::vec3(-1.7f,  3.0f, -7.5f),
-	  glm::vec3(1.3f, -2.0f, -2.5f),
-	  glm::vec3(1.5f,  2.0f, -2.5f),
-	  glm::vec3(1.5f,  0.2f, -1.5f),
-	  glm::vec3(-1.3f,  1.0f, -1.5f)
-	};
+	glm::vec3 lightColor(1.0f, 1.0f, 1.0f);
+	glm::vec3 toyColor(1.0f, 0.5f, 0.31f);
+	glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
 
     while(!glfwWindowShouldClose(pwin)){
 		float currentFrame = static_cast<float>(glfwGetTime());
@@ -253,33 +211,37 @@ int main() {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // also clear the depth buffer now!
 
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, textureId[0]);
-		glActiveTexture(GL_TEXTURE1);
-		glBindTexture(GL_TEXTURE_2D, textureId[1]);
-
-		shader.use();
+		objShader.use();
 
 		glm::mat4 projection = glm::perspective((float)glm::radians(camera.Zoom), (float)WINDOW_WIDTH / (float)WINDOW_HEIGHT, 0.1f, 100.0f);
 
-		float radius = 30.0f;
-		float camX = static_cast<float>(sin(glfwGetTime()) * radius);
-		float camZ = static_cast<float>(cos(glfwGetTime()) * radius);
 		glm::mat4 view = camera.GetViewMatrix();
 
-		shader.setMat4("view", view);
-		shader.setMat4("projection", projection);
+		objShader.setMat4("view", view);
+		objShader.setMat4("projection", projection);
 
         glBindVertexArray(vao); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-		for (unsigned int i = 0; i < 10; i++){
-			glm::mat4 model = glm::mat4(1.0f);
-			model = glm::translate(model, cubePositions[i]);
-			float angle = 20.0f * i + 20;
-			model = glm::rotate(model, glm::radians(angle) * (float)glfwGetTime(), glm::vec3(1.0f, 0.3f, 0.5f));
-			shader.setMat4("model", model);
+		glm::mat4 model = glm::mat4(1.0f);
+		model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f));
+		model = glm::rotate(model, glm::radians(10.0f) * (float)glfwGetTime(), glm::vec3(1.0f, 0.3f, 0.5f));
+		objShader.setMat4("model", model);
 
-			glDrawArrays(GL_TRIANGLES, 0, 36);
-		}
+		objShader.setVec3("objectColor", toyColor.r, toyColor.g, toyColor.b);
+		objShader.setVec3("lightColor", lightColor.r, lightColor.g, lightColor.b);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+
+		lightShader.use();
+		lightShader.setVec3("lightColor", lightColor.r, lightColor.g, lightColor.b);
+
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, glm::vec3(-5.0f, 5.0f, -10.0f));
+
+		lightShader.setMat4("view", view);
+		lightShader.setMat4("projection", projection);
+		lightShader.setMat4("model", model);
+
+		glBindVertexArray(lightVao);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
 
         glfwSwapBuffers(pwin);
         glfwPollEvents();    
