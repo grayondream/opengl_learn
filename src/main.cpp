2e9c17fc0d82fa153c2687982c2e38f2c62cfcb6
grayondream <grayondream@outlook.com>
2022-01-23 23:46:11 +0800

    feat: 添加材质和不同光的不同属性

--- src/main.cpp
+++ src/main.cpp
@@ -236,6 +236,20 @@ int main() {
 		objShader.setVec3("material.diffuse", 1.0f, 0.5f, 0.31f);
 		objShader.setVec3("material.specular", 0.5f, 0.5f, 0.5f);
 		objShader.setFloat("material.shininess", 32.0f);
+
+		glm::vec3 lightColor;
+		lightColor.x = sin(glfwGetTime() * 2.0f);
+		lightColor.y = sin(glfwGetTime() * 0.7f);
+		lightColor.z = sin(glfwGetTime() * 1.3f);
+
+		glm::vec3 diffuseColor = lightColor * glm::vec3(0.5f); // 降低影响
+		glm::vec3 ambientColor = diffuseColor * glm::vec3(0.2f); // 很低的影响
+
+		objShader.setVec3("light.ambient", ambientColor);
+		objShader.setVec3("light.diffuse", diffuseColor);
+
+		objShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
+
 		glDrawArrays(GL_TRIANGLES, 0, 36);
 
 		lightShader.use();
